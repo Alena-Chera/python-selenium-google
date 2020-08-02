fbf75e705eb58779c420b7a325413638becec7db
Alena <alenaryzhko1@gmail.com>
2020-08-01 20:39:15 -0400

    added logger

--- sample_script.py
+++ sample_script.py
@@ -7,18 +7,25 @@ driver = webdriver.Chrome(executable_path='drivers/chromedriver')
 # open the url
 driver.get('https://www.google.com/')
 
+# find element "search field"
 search = driver.find_element(By.NAME, 'q')
 search.clear()
 search.send_keys('Lamp')
 
-# wait 3 sec
+# wait 4 sec
 driver.implicitly_wait(4)
 
 # click search
 driver.find_element(By.NAME, 'btnK').click()
 
-# verify
+# verify word what we search is on the text on the page, that means we opened right page
 assert 'Lamp' in driver.find_element(By.XPATH, "//div[contains(@class,'commercial-unit-desktop-top')]").text
 assert 'Lamp' in driver.find_element(By.XPATH, "//div[@class='g']").text
 
+# verify the title matches to the expected
+browser_title = driver.title
+print(browser_title)
+assert "Lamp" in browser_title
+
+# close browser
 driver.quit()
